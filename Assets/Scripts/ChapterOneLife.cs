04754aef7ba8e247d43046bfef542f989bd98eb5
Scott Bromander <scott@primeacademy.io>
2016-10-21 20:22:00 -0500

    initial commit

---
+++ Assets/Scripts/ChapterOneLife.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChapterOneLife : MonoBehaviour {
+
+	public string Player = "";
+	public int PlayerHealth = 100;
+	public Vector3 Position = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (PlayerHealth == 100) {
+			Debug.Log ("Player is at Full Health");
+		}
+	}
+}
