399a0684abdf5e45c010744314023946322d1db7
Scott Bromander <scott@primeacademy.io>
2016-10-22 21:36:23 -0500

    enum and switch case

---
+++ Assets/Scripts/ChapterOneEnemyStates.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChapterOneEnemyStates : MonoBehaviour {
+
+	public enum EnemyState {
+		CHASE,
+		FLEE,
+		FIGHT,
+		HIDE
+	};
+
+	public EnemyState ActiveState = EnemyState.CHASE;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (ActiveState) {
+		case EnemyState.FIGHT:
+			{
+				Debug.Log ("Entering Fight State");
+			}
+			break;
+
+		case EnemyState.FLEE:
+		case EnemyState.HIDE:
+			{
+				Debug.Log ("Entered flee or hide state");
+			}
+			break;
+		default:
+			{
+				Debug.Log ("Entered chase state");
+			}
+			break;
+		}
+	}
+}
