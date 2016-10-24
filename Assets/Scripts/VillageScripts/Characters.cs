630c22cc6f4f2b201b4f732e50e431f8c3770614
Scott Bromander <scott@primeacademy.io>
2016-10-23 20:34:27 -0500

    village and characters scripts

---
+++ Assets/Scripts/VillageScripts/Characters.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Characters {
+
+	public string CharName = "";
+	public int Health = 100;
+	public int Strength = 100;
+	public float Speed = 10.0f;
+	public bool isAwake = true;
+
+	public virtual void SayGreeting(){
+		Debug.Log ("Hello, my friend");
+	}
+}
+
+public class ManCharacter : Characters {
+	public override void SayGreeting(){
+		Debug.Log ("Hello, I am man!");
+	}
+}
+
+public class WomanCharacter : Characters {
+	public override void SayGreeting(){
+		Debug.Log ("Hello, I am woman!");
+	}
+}
+
+public class OrcCharacter : Characters {
+	public override void SayGreeting(){
+		Debug.Log ("Hello, I am orc!");
+	}
+}
---
+++ Assets/Scripts/VillageScripts/Village.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Village : MonoBehaviour {
+
+	public Characters[] characters = null;
+
+	// Use this for initialization
+	void Start () {
+		characters = new Characters[5];
+
+		characters [0] = new ManCharacter ();
+		characters [1] = new WomanCharacter ();
+		characters [2] = new OrcCharacter ();
+		characters [3] = new ManCharacter ();
+		characters [4] = new WomanCharacter ();
+
+		EnterTavern ();
+	}
+
+	public void EnterTavern(){
+		foreach (Characters Char in characters) {
+			Char.SayGreeting ();
+		}
+	}
+}
