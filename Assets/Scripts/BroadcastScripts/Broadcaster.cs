495e1d4013e7d7b685f8a47fbd2e2e0a1f7c3bcd
Scott Bromander <scott@primeacademy.io>
2016-10-26 21:17:07 -0500

    SendMessage functionality and test

---
+++ Assets/Scripts/BroadcastScripts/Broadcaster.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Broadcaster : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SendMessage ("MyFunction", SendMessageOptions.DontRequireReceiver);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
