d870728c4b529572c13bd47c7dbf08c7bcb550f5
Scott Bromander <scott@primeacademy.io>
2016-10-28 19:40:36 -0500

    clean up on debug ogre

--- Assets/Scripts/DebugCode/EnemyOgre.cs
+++ Assets/Scripts/DebugCode/EnemyOgre.cs
@@ -7,10 +7,19 @@ public class EnemyOgre : MonoBehaviour {
 	public AttackType CurrentAttack = AttackType.PUNCH;
 
 	public int Health = 100;
+	public float RecoveryTime = 1.0f;
+	public float Speed = 1.0f;
+	public string OgreName = "Harry";
+
+	public override string ToString ()
+	{
+		return string.Format ("***Class EnemyOgre*** OgreName: {0} | Health: {1} | Speed: {2} | CurrentAttack: {3} | RecoveryTime: {4}", 
+			OgreName, Health, Speed, CurrentAttack, RecoveryTime);
+	}
 
 	// Use this for initialization
 	void Start () {
-	
+		Debug.Log (ToString ());
 	}
 	
 	// Update is called once per frame
