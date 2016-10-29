72b7536ea5dfb196ec19bf587fc8ee9f16e55a67
Scott Bromander <scott@primeacademy.io>
2016-10-28 19:57:00 -0500

    scripting symbol definitions test for compole conditional statements

---
+++ Assets/Scripts/DebugCode/CubeScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		#if SHOW_DEBUG_MESSAGES
+		Debug.Log("Pos: " + transform.position.ToString());
+		#endif
+
+		Debug.Log ("Start ran");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
