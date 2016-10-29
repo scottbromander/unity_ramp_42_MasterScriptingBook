725f41d1044cd37e886b914a7eaa4419eef87b62
Scott Bromander <scott@primeacademy.io>
2016-10-28 19:33:42 -0500

    debug ogre

---
+++ Assets/Scripts/DebugCode/EnemyOgre.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyOgre : MonoBehaviour {
+
+	public enum AttackType {PUNCH, MAGIC, SWORD, SPEAR};
+	public AttackType CurrentAttack = AttackType.PUNCH;
+
+	public int Health = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
