52053deb25612f002c87b26315cc6ed8625d8b7f
Scott Bromander <scott@primeacademy.io>
2016-10-29 20:59:49 -0500

    debug draw test

---
+++ Assets/Scripts/DebugCode/GizmoCube.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GizmoCube : MonoBehaviour {
+
+	public bool DrawGizmos = true;
+
+	void OnDrawGizmos(){
+		if (!DrawGizmos)
+			return;
+
+		Gizmos.color = Color.blue;
+
+		Gizmos.DrawRay (transform.position, transform.forward.normalized * 4.0f);
+
+		Gizmos.color = Color.red;
+
+		Gizmos.DrawWireSphere (transform.position, 4.0f);
+
+		Gizmos.color = Color.white;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
