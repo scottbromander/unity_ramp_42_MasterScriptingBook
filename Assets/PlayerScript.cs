1220029108d97f105570ddb3c894cda8de9320e2
Scott Bromander <scott@primeacademy.io>
2016-11-01 21:07:35 -0500

    Playing with LineCasts

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public GameObject Enemy = null;
+	public LayerMask lm;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!Physics.Linecast (transform.position, Enemy.transform.position, lm)) {
+			Debug.Log ("PathClear");
+		}
+	}
+
+	void OnDrawGizmos(){
+		Gizmos.DrawLine(transform.position, Enemy.transform.position);
+	}
+}
