d7a74107b51ac25bd1b10b7b98e500ac6394101f
Scott Bromander <scott@primeacademy.io>
2016-11-02 21:40:17 -0500

    experimenting with parent assignment at runtime

---
+++ Assets/Scripts/ChapterThree/ChildEnemy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChildEnemy : MonoBehaviour {
+
+	private GameObject parent;
+
+	// Use this for initialization
+	void Start () {
+		parent = GameObject.Find ("Player");
+		transform.parent = parent.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
