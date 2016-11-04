d36b3134b5c9b63fceb12a3ee1fa1408ae916111
Scott Bromander <scott@primeacademy.io>
2016-11-03 21:07:36 -0500

    movement tests

---
+++ Assets/Scripts/ChapterThree/BasicEnemyMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicEnemyMovement : MonoBehaviour {
+
+	public float AmountToMove = 1.0f;
+	private float _amountMoved = 0.0f;
+	private bool _moveBack = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var movement = AmountToMove;
+
+		if (_amountMoved > 50.0f) {
+			_moveBack = !_moveBack;
+			_amountMoved = 0;
+		}
+
+		if (_moveBack) {
+			movement *= 1;
+		} else {
+			movement *= -1;
+		}
+
+		transform.localPosition += new Vector3 (0, 0, movement);
+
+		Debug.Log ("Amount Moved: " + _amountMoved);
+		_amountMoved += Mathf.Abs(movement);
+	}
+}
