341e07988f6e3e524f78210419ef13955049fc36
Scott Bromander <scott@primeacademy.io>
2016-11-07 20:27:13 -0600

    Immortal Game Controller Test

---
+++ Assets/Scripts/ChapterThree/ChapterThreeGameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChapterThreeGameManager : MonoBehaviour {
+
+	public static ChapterThreeGameManager Instance {
+		get {
+			return Instance;
+		}
+	}
+
+	private static ChapterThreeGameManager instance = null;
+
+	public int HighScore = 0;
+	public bool IsPaused = false;
+	public bool InputAllowed = true;
+
+	void Awake (){   
+		 if(instance){
+		   DestroyImmediate(gameObject);
+		   return;
+		 }
+
+		 //Make this active and only instance
+		 instance = this;
+
+		 //Make game manager persistent
+		 DontDestroyOnLoad(gameObject);
+	}
+}
