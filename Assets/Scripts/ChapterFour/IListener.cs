c64a2f07b1a7b89134664a7b537ae3539640b317
Scott Bromander <scott@primeacademy.io>
2016-11-13 21:20:23 -0600

    Ilistener interface

---
+++ Assets/Scripts/ChapterFour/IListener.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum EVENT_TYPE {
+	GAME_INIT,
+	GAME_END,
+	AMMO_EMPTY,
+	HEALTH_CHANGE,
+	DEAD
+};
+
+public interface IListener {
+	void OnEvent(EVENT_TYPE Event_Type, Component Sender, Object Param = null);
+}
