07367914b88d24c386b681f45885c1d468c8c31e
Scott Bromander <scott@primeacademy.io>
2016-11-13 21:22:30 -0600

    Custom listener setup

---
+++ Assets/Scripts/ChapterFour/MyCustomListener.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCustomListener : MonoBehaviour, IListener {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnEvent (EVENT_TYPE Event_Type, Component Sender, Object Param = null) {
+
+	}
+}
