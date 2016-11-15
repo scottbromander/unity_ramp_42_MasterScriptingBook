9ce812f78ed3cfe85ae72f338fe7079ee2c287c2
Scott Bromander <scott@primeacademy.io>
2016-11-14 20:14:42 -0600

    building out Event Manager

--- Assets/Scripts/ChapterFour/EventManager.cs
+++ Assets/Scripts/ChapterFour/EventManager.cs
@@ -1,7 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class EventManager : MonoBehaviour {
+
+	#region C# properties
+
+	public static EventManager Instance {
+		get {return instance;}
+		set { }
+	}
+	#endregion
+
+	#region variables
+	private static EventManager instance = null;
+
+	private Dictionary<EVENT_TYPE, List<IListener>> Listeners = new Dictionary<EVENT_TYPE, List<IListener>>();
+	#endregion
+
+	#region methods
+
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+			DontDestroyOnLoad (gameObject);
+		} else {
+			DestroyImmediate (this);
+		}
+	}
+
+	public void AddListener(EVENT_TYPE Event_Type, IListener Listener){
+		List<IListener> ListenList = null;
+
+		if(Listeners.TryGetValue(Event_Type, out ListenList)){
+			ListenList.Add(Listener);
+			return;
+		}
+
+		ListenList = new List<IListener>();
+		ListenList.Add(Listener);
+		ListenList.Add(Event_Type, ListenList);
+	}
 
 	// Use this for initialization
 	void Start () {
